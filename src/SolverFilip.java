1e2531eac2de0ef23a4fe7914cf611ad3011dd6d
FilipMolnar <F.M.T.Molnar@student.tudelft.nl>
2024-03-22 08:53:05 +0100

    fix basic combinatorics.. FUNGUJE!

--- src/SolverFilip.java
+++ src/SolverFilip.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solver {
+class SolverFilip {
 
     static class SudokuConstraint extends Constraint{
         Variable var;
@@ -271,6 +271,19 @@ class Solver {
                 size = variables[i].domains.getLast().size();
             }
         }
+        // for(int i=0; i<variables.length; i++){
+        //     if(!usedPlace[i]) {
+        //         idx = i;
+        //         size = variables[i].domains.getLast().size();
+        //         break;
+        //     }
+        // }
+        // for(int i=idx+1; i<variables.length; i++){
+        //     if(!usedPlace[i] && variables[i].domains.getLast().size()<size) {
+        //         idx = i;
+        //         size = variables[i].domains.getLast().size();
+        //     }
+        // }
         return idx;
     }
 
@@ -360,6 +373,113 @@ class Solver {
             return false;
         }
         return true;
+    }
+
+    void search2(boolean findAllSolutions) {
+        Deque<VariableAssigned> tmp_sol = new LinkedList<>();
+        Deque<VariableAssigned> assStack = new LinkedList<>();
+        PriorityQueue<VariablePQ> pq = new PriorityQueue<>();
+        boolean[] usedPlace = new boolean[variables.length];
+        
+        //init pq
+        for(int i=0; i<variables.length; i++){
+            pq.add(new VariablePQ(variables[i].index, variables[i].domains.getLast().size()));
+            usedPlace[i] = false;
+        }
+        //init assStack
+        VariablePQ firstPQ = pq.remove();
+
+        for(int i=0; i<variables[firstPQ.index].domains.getLast().size(); i++){
+            assStack.addLast(new VariableAssigned(firstPQ.index, variables[firstPQ.index].domains.getLast().get(i)));
+        }
+
+        while(!assStack.isEmpty()){
+            VariableAssigned var = assStack.removeLast();
+            if(usedPlace[var.index]){
+                // printSol(tmp_sol);
+                VariableAssigned last = tmp_sol.getLast();
+                if(tmp_sol.size() == variables.length && tmp_sol.getLast().index == var.index){
+                    usedPlace[last.index] = false;
+                    tmp_sol.removeLast();
+                } else {
+                    if(tmp_sol.size() == variables.length){
+                        pq.add(new VariablePQ(last.index, variables[last.index].domains.getLast().size()));    
+                        usedPlace[last.index] = false;
+                        tmp_sol.removeLast();
+                    }
+                    while(tmp_sol.getLast().index != var.index){
+                        usedPlace[tmp_sol.getLast().index] = false;
+                        repropagate2(tmp_sol.getLast(), pq, usedPlace);
+                        pq.add(new VariablePQ(tmp_sol.getLast().index, variables[tmp_sol.getLast().index].domains.getLast().size()));
+                        tmp_sol.removeLast();
+                    }
+                    // printSol(tmp_sol);
+    
+                    if(tmp_sol.getLast().index == var.index){
+                        usedPlace[tmp_sol.getLast().index] = false;
+                        repropagate2(tmp_sol.getLast(), pq, usedPlace);
+                        
+                        tmp_sol.removeLast();
+                    }   
+                }
+            }
+
+            tmp_sol.addLast(var);
+            usedPlace[var.index] = true;
+            int size = tmp_sol.size();
+
+            //found solution
+            if(size == variables.length){
+                // printSol(tmp_sol);
+                solutions.add(convertToArray(tmp_sol));
+                if(findAllSolutions == false)
+                    return;
+                continue;
+            }
+            if(!propagate2(tmp_sol, pq, usedPlace)){
+                continue;
+            }
+            if(pq.size() == 0) continue;
+            VariablePQ smallestVariable = pq.remove();
+            if(usedPlace[smallestVariable.index]) continue;
+            
+            List<Integer> dom = variables[smallestVariable.index].domains.getLast();
+            for(int i=0; i<dom.size(); i++){
+                assStack.addLast(new VariableAssigned(smallestVariable.index, dom.get(i)));
+            }
+        }
+    }
+    boolean propagate2(Deque<VariableAssigned> sol, PriorityQueue<VariablePQ> pq, boolean[] usedPlace){
+        List<Integer> affected = variables[sol.getLast().index].affects;
+        for(int a : affected){
+            if(!usedPlace[a]){
+                VariablePQ newVar = new VariablePQ(variables[a].index, variables[a].domains.getLast().size());
+                if(pq.remove(newVar)){
+                    constraints[a].infer(sol);
+                    newVar.domainSize = variables[a].domains.getLast().size();
+                    pq.add(newVar);
+                    if(newVar.domainSize == 0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    void repropagate2(VariableAssigned last, PriorityQueue<VariablePQ> pq, boolean[] usedPlace){
+        List<Integer> affected = variables[last.index].affects;
+        for(int a : affected){
+            Deque<List<Integer>> domains = variables[a].domains;
+            if(domains.size() > 1 && !usedPlace[a]){
+                VariablePQ newVar = new VariablePQ(a, domains.getLast().size());
+                domains.removeLast();
+                if(pq.remove(newVar)){
+                    newVar.domainSize = domains.getLast().size();
+                    pq.add(newVar);
+                }
+            }
+        }
     }
 
     /**
